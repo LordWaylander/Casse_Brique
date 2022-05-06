e1f82a0b12a57fa2826129428749e0a193b10b72
Waylander <francois.caizes@laposte.net>
2022-05-06 18:15:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,33 +25,39 @@ let config = {
 }
 
 class Message extends Phaser.GameObjects.Text  {
-    constructor(game, w, h, message) {
+    constructor(game, message) {
         super(game);
-        this.width = w;
+        this.width = game.physics.world.bounds.width/2;
         this.height = game.physics.world.bounds.height/2;
-        this.message = message; 
+        this.message = message;
+        this.addText = game.add;
     }
+    
     msg() {
-        this.width,
-        this.height,
-        this.message,
-        {
-          fontFamily: 'Monaco, Courier, monospace',
-          fontSize: '50px',
-          fill: '#000'
-        }
+        let text = this.addText.text(
+            this.width,
+            this.height,
+            this.message,
+            {
+                fontFamily: 'Monaco, Courier, monospace',
+                fontSize: '50px',
+                fill: '#000'
+            },
+        );
+        text.setOrigin(0.5);
+        text.setVisible(false);
+
+        return text; 
     }
 }
 
 let ball;
 let paddle;
-let cursors;
 let play = false;
 
 function preload() {
     this.load.image('ball', 'img/ball.png');
-    this.load.image('paddle', 'img/paddle.png');
-    
+    this.load.image('paddle', 'img/paddle.png');  
 }
 
 function create() {
@@ -64,98 +70,48 @@ function create() {
     paddle = this.physics.add.image(config.width*0.5, config.height-20, 'paddle');
     paddle.setCollideWorldBounds(true);
     paddle.setImmovable(true);
-    
-    
-    if (play === true) {
-        this.physics.add.collider(ball, paddle, hitPlayer, null, this);
-    }else {
-        this.physics.add.collider(ball, paddle);
-    }
-    
 
+    this.physics.add.collider(ball, paddle, hitPaddle, null, this);
+    
     //désactive collision monde bas
     this.physics.world.checkCollision.down = false;
 
-    /*gameOverText = this.add.text(
-        this.physics.world.bounds.width / 2,
-        this.physics.world.bounds.height / 2,
-        'Game Over',
-        {
-          fontFamily: 'Monaco, Courier, monospace',
-          fontSize: '50px',
-          fill: '#000'
-        },
-    );*/
-    gameOverText = this.add.text(new Message(this, this.physics.world.bounds.width/2, this.physics.world.bounds.width/2, 'game over').msg());
-        
-    gameOverText.setOrigin(0.5);
-    gameOverText.setVisible(false);
-    console.log(gameOverText);
-
-    gameWinText = this.add.text(
-        this.physics.world.bounds.width/2,
-        this.physics.world.bounds.height/2,
-        'WIN',
-        {
-            fontFamily: 'Monaco, Courier, monospace',
-            fontSize: '50px',
-            fill: '#000'
-        },
-    );
-    gameWinText.setOrigin(0.5);
-    gameWinText.setVisible(false);
-
-    ball.body.onWorldBounds = true;
-    ball.body.onOverlap = true;
-    ball.body.onCollide = true;
+    gameOverText = new Message(this, 'Game Over').msg();
+    gameWinText = new Message(this, 'You Win !').msg();
 }
 
 function update() {
-    /*if (cursors.left.isDown) {
-        //paddle.setVelocityX(-160);
-        //paddle.scrollX -= 2
-        paddle.x -= 5;
-    } else if (cursors.right.isDown) {
-        paddle.x += 5;
-    }*/
-    // follow mouse
-    //paddle.x = this.input.x;
-   //console.log(this.input.mouse.onMouseMove);
-
     this.input.on('pointermove', function(pointer) {
         paddle.x = pointer.position.x
     }, this);
 
     this.input.on('pointerdown', function (pointer) {
         play = true;
+        
         if(pointer.button == 0) {
-            let randomWidth = Phaser.Math.Between(0, 800)
-            ball.setVelocity(randomWidth, -350);
+            let velocityX = Phaser.Math.Between(0, 400)
+            ball.setVelocity(velocityX, -350);
         }
     },this);
     
     if (ball.y > config.height) {
-        console.log(gameOverText);
         gameOverText.visible = true;
         ball.disableBody(true, true);
-        console.log(gameOverText);
-        //console.log(this.anims.paused);
-        //this.sys.scene.game.isRunning = false;
         this.scene.pause("default");
-        //return;
-    }
-    
+    } 
 }
 
-function hitPlayer(ball, player) {
-    ball.setVelocityY(ball.body.velocity.y - 5);
-    let newXVelocity = Math.abs(ball.body.velocity.x) + 5;
-
-    if (ball.x < player.x) {
-        ball.setVelocityX(-newXVelocity);
-      } else {
-        ball.setVelocityX(newXVelocity);
-      }
+function hitPaddle(ball, paddle) {
+    if (play === true) {
+        ball.setVelocityY(ball.body.velocity.y - 5);
+        let newXVelocity = Math.abs(ball.body.velocity.x) + 5;
+    
+        if (ball.x < paddle.x) {
+            ball.setVelocityX(-newXVelocity);
+        } else {
+            ball.setVelocityX(newXVelocity);
+        }
+    } 
 }
 
 var game = new Phaser.Game(config);
